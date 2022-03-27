6cf99f0b38353395fe87811a97192834db3561be
Matthias Nannt <mail@matthiasnannt.com>
2022-03-27 10:47:22 +0200

    add moincoin

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,27 +34,28 @@ import styles from "../styles/Home.module.css";
   },
 ]; */
 
-export default function Example() {
+export default function Home() {
   return (
     <div className="bg-white">
       <main>
         {/* Hero section */}
-        <div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-48">
-          <div className="flex items-center w-full justify-center">
-            <div className={styles.logo}></div>
+        <div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-40">
+          <div className="flex items-center justify-center w-full">
+            {/*  <div className={styles.logo}></div> */}
+            <img src="/img/moincoin.png" className="h-56" />
           </div>
           <div className="flex justify-center w-full px-4 sm:px-6 lg:px-8">
-            <div className="mt-20">
+            <div className="mt-10">
               <div className="flex justify-center">
                 <span className="rounded bg-sky-50 px-2.5 py-1 text-xs font-semibold text-sky-500 tracking-wide uppercase">
                   Neu im Norden
                 </span>
               </div>
               <div className="mt-6 sm:max-w-5xl">
-                <h1 className="text-4xl font-extrabold text-center text-gray-900 tracking-tight sm:text-5xl">
+                <h1 className="text-4xl font-extrabold tracking-tight text-center text-gray-900 sm:text-5xl">
                   NFT-Projekte aus Norddeutschland
                 </h1>
-                <p className="mt-6 text-xl text-gray-500 text-center">
+                <p className="mt-6 text-xl text-center text-gray-500">
                   Wir bringen das NFT-Ökosystem in Norddeutschland auf ein neues
                   Niveau.
                   <br />
@@ -63,18 +64,18 @@ export default function Example() {
                   NFT-Enthusiasten kennen und verpasse keine News zu moinNFT.
                 </p>
               </div>
-              <div className="mt-8 grid grid-cols-2 gap-6">
+              <div className="grid grid-cols-2 gap-6 mt-8">
                 <a
                   href="https://discord.gg/UUHfRdPywv"
                   target="_blank"
                   rel="noreferrer"
-                  className="block rounded-md border text-center border-transparent px-5 py-3 bg-sky-500 text-base font-medium text-white shadow hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 sm:px-10"
+                  className="block px-5 py-3 text-base font-medium text-center text-white border border-transparent rounded-md shadow bg-sky-500 hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 sm:px-10"
                 >
                   Zur Discord Community
                 </a>
                 <a
                   href="#about"
-                  className="block rounded-md px-5 py-3 text-center bg-white text-base font-medium text-sky-500 border border-sky-500 shadow hover:bg-sky-100 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 sm:px-10"
+                  className="block px-5 py-3 text-base font-medium text-center bg-white border rounded-md shadow text-sky-500 border-sky-500 hover:bg-sky-100 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 sm:px-10"
                 >
                   Mehr erfahren
                 </a>
@@ -91,9 +92,9 @@ export default function Example() {
                 aria-hidden="true"
                 className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen"
               >
-                <div className="absolute inset-y-0 right-1/2 w-full bg-gray-50 rounded-r-3xl lg:right-72" />
+                <div className="absolute inset-y-0 w-full right-1/2 bg-gray-50 rounded-r-3xl lg:right-72" />
                 <svg
-                  className="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12"
+                  className="absolute -ml-3 top-8 left-1/2 lg:-right-8 lg:left-auto lg:top-12"
                   width={404}
                   height={392}
                   fill="none"
@@ -125,11 +126,11 @@ export default function Example() {
                   />
                 </svg>
               </div>
-              <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
+              <div className="relative max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
                 {/* Testimonial card*/}
-                <div className="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
+                <div className="relative pt-64 pb-10 overflow-hidden shadow-xl rounded-2xl">
                   <img
-                    className="absolute inset-0 h-full w-full object-cover"
+                    className="absolute inset-0 object-cover w-full h-full"
                     src="/img/nft-image.jpg"
                     alt=""
                   />
@@ -139,7 +140,7 @@ export default function Example() {
                     <blockquote className="mt-8">
                       <div className="relative text-lg font-medium text-white md:flex-grow">
                         <svg
-                          className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-sky-400"
+                          className="absolute top-0 left-0 w-8 h-8 transform -translate-x-3 -translate-y-2 text-sky-400"
                           fill="currentColor"
                           viewBox="0 0 32 32"
                           aria-hidden="true"
@@ -163,13 +164,13 @@ export default function Example() {
               </div>
             </div>
 
-            <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
+            <div className="relative max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:px-0">
               {/* Content area */}
               <div className="pt-12 sm:pt-16 lg:pt-20">
-                <h2 className="text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                   Unsere Mission
                 </h2>
-                <div className="mt-6 text-gray-500 space-y-6">
+                <div className="mt-6 space-y-6 text-gray-500">
                   <p className="text-base leading-7">
                     moinNFT befindet sich im Aufbau eines digitalen
                     Schaufensters für Kunst- und NFT-Projekte aus
@@ -208,7 +209,7 @@ export default function Example() {
                   {stats.map((stat) => (
                     <div
                       key={stat.label}
-                      className="border-t-2 border-gray-100 pt-6"
+                      className="pt-6 border-t-2 border-gray-100"
                     >
                       <dt className="text-base font-medium text-gray-500">
                         {stat.label}
@@ -232,13 +233,13 @@ export default function Example() {
 
         {/* Logo cloud section */}
         {/*  <div className="mt-32">
-          <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+          <div className="max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
             <div className="lg:grid lg:grid-cols-2 lg:gap-24 lg:items-center">
               <div>
-                <h2 className="text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                   Backed by world-renowned investors
                 </h2>
-                <p className="mt-6 max-w-3xl text-lg leading-7 text-gray-500">
+                <p className="max-w-3xl mt-6 text-lg leading-7 text-gray-500">
                   Sagittis scelerisque nulla cursus in enim consectetur quam.
                   Dictum urna sed consectetur neque tristique pellentesque.
                   Blandit amet, sed aenean erat arcu morbi. Cursus faucibus nunc
@@ -255,7 +256,7 @@ export default function Example() {
                 {logos.map((logo) => (
                   <div
                     key={logo.name}
-                    className="col-span-1 flex justify-center py-8 px-8 bg-gray-50"
+                    className="flex justify-center col-span-1 px-8 py-8 bg-gray-50"
                   >
                     <img className="max-h-12" src={logo.url} alt={logo.name} />
                   </div>
@@ -270,7 +271,7 @@ export default function Example() {
           <div aria-hidden="true" className="hidden sm:block">
             <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50 rounded-r-3xl" />
             <svg
-              className="absolute top-8 left-1/2 -ml-3"
+              className="absolute -ml-3 top-8 left-1/2"
               width={404}
               height={392}
               fill="none"
@@ -302,14 +303,14 @@ export default function Example() {
               />
             </svg>
           </div>
-          <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-            <div className="relative rounded-2xl px-6 py-10 bg-sky-500 overflow-hidden shadow-xl sm:px-12 sm:py-20">
+          <div className="max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="relative px-6 py-10 overflow-hidden shadow-xl rounded-2xl bg-sky-500 sm:px-12 sm:py-20">
               <div
                 aria-hidden="true"
                 className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0"
               >
                 <svg
-                  className="absolute inset-0 h-full w-full"
+                  className="absolute inset-0 w-full h-full"
                   preserveAspectRatio="xMidYMid slice"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -329,10 +330,10 @@ export default function Example() {
               </div>
               <div className="relative">
                 <div className="sm:text-center">
-                  <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">
+                  <h2 className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
                     Komm&apos; in unsere Community!
                   </h2>
-                  <p className="mt-6 mx-auto max-w-2xl text-lg text-sky-100">
+                  <p className="max-w-2xl mx-auto mt-6 text-lg text-sky-100">
                     Tausche dich mit uns aus, lerne mit uns, baue mit uns. In
                     unserer moinNFT Discord Community versammeln sich die NFT
                     Interessierten aus dem Norden mit regelmäßigen virutellen
@@ -346,7 +347,7 @@ export default function Example() {
                     href="https://discord.gg/UUHfRdPywv"
                     target="_blank"
                     rel="noreferrer"
-                    className="block w-full rounded-md border text-center border-transparent px-5 py-3 bg-gray-900 text-base font-medium text-white shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-sky-500 sm:px-10"
+                    className="block w-full px-5 py-3 text-base font-medium text-center text-white bg-gray-900 border border-transparent rounded-md shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-sky-500 sm:px-10"
                   >
                     Zur moinNFT Discord Community 🤗
                   </a>
